6793c072ebe5a6e05830a0e20eb62457d43d9b16
maywascold <stepanovigor2110@gmail.com>
2024-06-03 14:29:16 +0300

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Playfair_Display } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const playfair = Playfair_Display({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${playfair.className} overflow-x-hidden`}>{children}</body>
     </html>
   );
 }
